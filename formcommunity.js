9515f5dbf3658339da0639166a2bf9c5f5c1c592
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2022-09-24 14:34:28 +0530

    Add files via upload

---
+++ formcommunity.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Formcommunity() {
+    return(
+        <div>
+            <h1>Hi Register community page goes here</h1>
+        </div>
+
+    );
+}
+export default Formcommunity;
