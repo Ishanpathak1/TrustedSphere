631d8b6d45eac4667ec43f7f0c0912a60e378838
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2022-09-25 16:40:52 +0530

    Add files via upload
    After adding the REQUIRED tag in the forms input type tag

--- formforuserinp.js
+++ formforuserinp.js
@@ -1,18 +1,50 @@
 import React from "react";
 import './formuserinp.css';
 import saly from "./images/Saly-7.png";
+import {useState} from "react";
 function FormUserInp() {
+    const[name,setName]=useState('Enter Your Name Here')
+    const[Address1,setAddress1]=useState('Address Line 1')
+    const[Address2,setAddress2]=useState('Address Line 2')
+    const[City,setCity]=useState('City')
+    const[State,setState]=useState('State')
+    const[Country,setCountry]=useState('Country')
+    const[NooP,setNoop]=useState('Enter Your Number Here')
+    function getNoop(val){
+        setNoop(val.target.value)
+    }
+    function getName(val)
+    {
+        setName(val.target.value)
+
+    }
+    function getCountry(val){
+        setCountry(val.target.value)
+    }
+    function getState(val){
+        setState(val.target.value)
+    }
+    function getCity(val){
+        setCity(val.target.value)
+    }
+    function getAddress2(val){
+        setAddress2(val.target.value)
+    }
+    function getAddress1(val){
+        setAddress1(val.target.value)
+    }
     return (
         <div>
              
         <form action="">
+            
             <div className="ForNo">
                <div className="ForNoText"> <label htmlFor="Noofprop">Number of property</label></div>
-                <input type="text" name="NoofP" id="NoofP" className="ForNoInput"/>
+                <input type="text" required value={NooP} onChange={getNoop} name="NoofP" id="NoofP" className="ForNoInput"/>
             </div>
             <div className="ForName">
                 <div className="ForNameText"><label htmlFor="Name">Name</label></div>
-                <input type="text" name="Name" id="Name" className="ForNameInput"/>
+                <input type="text" required value={name} onChange={getName} name="Name" id="Name" className="ForNameInput"/>
             </div>
             <div className="ForAmount">
                 
@@ -23,15 +55,15 @@ function FormUserInp() {
                 <input type="text" name="Uniq" id="Uniq" className="ForUniqInput"/>
             </div>
             <div className="RectangleForForm"></div>
-              <input type="text" name="Address1" id="Address1" className="ForAddressLine1"/>
-              <input type="text" name="Address2" id="Address2" className="ForAddressLine2"/>
-              <input type="text" name="City" id="City" className="ForCity"/>
-              <input type="text" name="State" id="State" className="ForState"/>
-              <input type="text" name="Country" id="Country" className="ForCountry"/>
+              <input type="text" required value={Address1} onChange={getAddress1} name="Address1" id="Address1" className="ForAddressLine1"/>
+              <input type="text" required value={Address2} onChange={getAddress2} name="Address2" id="Address2" className="ForAddressLine2"/>
+              <input type="text" required value={City} onChange={getCity} name="City" id="City" className="ForCity"/>
+              <input type="text" required value={State} onChange={getState}name="State" id="State" className="ForState"/>
+              <input type="text" required value={Country} onChange={getCountry} name="Country" id="Country" className="ForCountry"/>
               <button className="CYL"> Confirm Your Location </button>
               <button className="RegisterProperty"> Register Property </button>
               <div className="imagesaly">
-                 <img src={saly} alt="" width={622} height={602}/>
+                 <img src={saly} alt="3DImage" width={622} height={602}/>
               </div>
 
             
